8f479be3d8da79cd37968adcce30927b58f6a76c
root <root@xaldovah.localdomain>
2024-03-11 18:04:55 +0300

    Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,44 @@
+const redis = require('redis');
+
+class RedisClient {
+	constructor() {
+		this.client = redis.createClient();
+
+		this.client.on('error', (err) => {
+			console.error('Redis Client Error:', err);
+		});
+	}
+
+	isAlive() {
+		return this.client.connected;
+	}
+
+	async get(key) {
+		try {
+			const value = await this.client.get(key);
+			return value;
+		} catch (erro) {
+			console.error('Error getting key:', key, error);
+			return null;
+		}
+	}
+	
+	async set(key, value, duration) {
+		try {
+			await this.client.set(key, value, 'EX', duration);
+		} catch (error) {
+			console.error('Error setting key:', key, error);
+		}
+	}
+
+	async del(key) {
+		try {
+			await this.client.del(key);
+		} catch (error) {
+			console.error('Error deleting key:', key, error);
+		}
+	}
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
