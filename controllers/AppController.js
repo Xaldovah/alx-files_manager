bdcdb082f4e539720ef20f4f737c94a66ab99f07
root <root@xaldovah.localdomain>
2024-03-12 16:04:35 +0300

    First API

---
+++ controllers/AppController.js
@@ -0,0 +1,22 @@
+import redisClient from '../utils/redis.js';
+import dbClient from '../utils/db.js';
+
+export const getStatus = (req, res) => {
+	const redisAlive = redisClient.isAlive();
+	const dbAlive = dbClient.isAlive();
+
+	res.status(200).json({ redis: redisAlive, db: dbAlive });
+};
+
+
+export const getStats = async (req, res) => {
+	try {
+		const numUsers = await dbClient.nbUsers();
+		const numFiles = await dbClient.nbFiles();
+
+		res.status(200).json({ users: numUsers, files: nuFiles });
+	} catch (error) {
+		console.error('Error retrieving stats:', error);
+		res.status(500).json({ error: 'Internal server error' });
+	}
+};
