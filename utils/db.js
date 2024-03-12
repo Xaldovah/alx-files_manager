55ab018ba5d5545b66f98df73d399f3b1714232f
root <root@xaldovah.localdomain>
2024-03-12 15:28:44 +0300

    Mongo Utils

--- utils/db.js
+++ utils/db.js
@@ -1,37 +1,37 @@
 import { MongoClient } from 'mongodb';
 
 class DBClient {
-	constructor() {
-		const host = process.env.DB_HOST || 'localhost';
-		const port = process.env.DB_PORT || 27017;
-		const database = process.env.DB_DATABASE || 'files_manager';
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
 
-		const uri = `mongodb://${host}:${port}/${database}`;
-		this.client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-		this.client.connect((err) => {
-			if (err) {
-				console.error('MongoDB connection error:', err);
-			} else {
-				console.log('Connected to MongoDB');
-			}
-		});
-	}
+    const uri = `mongodb://${host}:${port}/${database}`;
+    this.client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    this.client.connect((err) => {
+      if (err) {
+        console.error('MongoDB connection error:', err);
+      } else {
+        console.log('Connected to MongoDB');
+      }
+    });
+  }
 
-	isAlive() {
-		return this.client.isConnected();
-	}
+  isAlive() {
+    return this.client.isConnected();
+  }
 
-	async nbUsers() {
-		const db = this.client.db();
-		const collection = db.collection('users');
-		return collection.countDocuments();
-	}
+  async nbUsers() {
+    const db = this.client.db();
+    const collection = db.collection('users');
+    return collection.countDocuments();
+  }
 
-	async nbFiles() {
-		const db = this.client.db();
-		const collection = db.collection('files');
-		return collection.countDocuments();
-	}
+  async nbFiles() {
+    const db = this.client.db();
+    const collection = db.collection('files');
+    return collection.countDocuments();
+  }
 }
 
 const dbClient = new DBClient();
