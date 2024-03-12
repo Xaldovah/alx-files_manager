a03a57aa776fdd8952347225426389fcee40d04d
root <root@xaldovah.localdomain>
2024-03-12 15:02:25 +0300

    Redis utils

--- utils/redis.js
+++ utils/redis.js
@@ -1,43 +1,42 @@
 const redis = require('redis');
 
-
 class RedisClient {
-	constructor() {
-		this.client = redis.createClient();
-		this.client.on('error', (err) => {
-			console.error('Redis Client Error:', err);
-		});
-	}
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (err) => {
+      console.error('Redis Client Error:', err);
+    });
+  }
 
-	isAlive() {
-		return this.client.connected;
-	}
+  isAlive() {
+    return this.client.connected;
+  }
 
-	async get(key) {
-		try {
-			const value = await this.client.get(key);
-			return value;
-		} catch (erro) {
-			console.error('Error getting key:', key, error);
-			return null;
-		}
-	}
-	
-	async set(key, value, duration) {
-		try {
-			await this.client.set(key, value, 'EX', duration);
-		} catch (error) {
-			console.error('Error setting key:', key, error);
-		}
-	}
+  async get(key) {
+    try {
+      const value = await this.client.get(key);
+      return value;
+    } catch (error) {
+      console.error('Error getting key:', key, error);
+      return null;
+    }
+  }
 
-	async del(key) {
-		try {
-			await this.client.del(key);
-		} catch (error) {
-			console.error('Error deleting key:', key, error);
-		}
-	}
+  async set(key, value, duration) {
+    try {
+      await this.client.set(key, value, 'EX', duration);
+    } catch (error) {
+      console.error('Error setting key:', key, error);
+    }
+  }
+
+  async del(key) {
+    try {
+      await this.client.del(key);
+    } catch (error) {
+      console.error('Error deleting key:', key, error);
+    }
+  }
 }
 
 const redisClient = new RedisClient();
